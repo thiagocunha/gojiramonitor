1dbd9eb11497e8a33ddd76bcee8d57fad9a3a04b
Thiago Chierici Cunha <tchierici@lnxcit019242.cit>
2019-10-01 17:03:28 -0300

    partial commit with infinite loop bug when requesting the comments

--- background.js
+++ background.js
@@ -62,21 +62,50 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 chrome.alarms.create("jiraMon", {
     delayInMinutes: 1,
-    periodInMinutes: 5
+    periodInMinutes: 1
 });
 
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
     if (alarm.name === "jiraMon") {
+        var koidsCIT = [
+            'S519148', 
+            'S511003', 
+            'S520108', 
+            'S492930', 
+            'S389051', 
+            'S385334', 
+            'S519789', 
+            'S385334', 
+            'S278379', 
+            'S466884', 
+            'O53275'
+        ];
         $.ajax("https://jira.coke.com/jira/rest/gadget/1.0/issueTable/filter?num=10&tableContext=jira.table.cols.dashboard&addDefault=false&columnNames=issuekey&columnNames=summary&columnNames=lastViewed&enableSorting=true&paging=true&showActions=true&filterId=31627&sortBy=&startIndex=0&_=1569954812196").done(function( data ) {
             // chrome.runtime.sendMessage({type: "notify", notifyTotal: indice.toString()});
             var r = new RegExp(/(?:issuekey=.(AEM-\d*))/, 'gm');
             var keys = [];
             var match = r.exec(data.table);
             while(match !== null){
-                console.log(match);
-                keys.push(match[1]);
-                match = r.exec(data.table);
+                var ticket = match[1];
+                console.log(ticket);
+                $.ajax("https://jira.coke.com/jira/browse/"+ticket+"?page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel&_=1569958096799").done(function( dataComments ) {
+                    var regComments = new RegExp(/(?:alt=.*?"(\w\d{5}).*?added)/, 'gm');
+                    var matchComments = regComments.exec(dataComments);
+                    var lastKOID = "";
+                    while(matchComments !== null){
+                        lastKOID = matchComments[1]
+                        matchComments = regComments.exec(dataComments);
+                    }
+                    if (koidsCIT.includes(lastKOID)){
+                        keys.push(ticket);
+                        console.log("Last comment not from CIT");
+                    }
+                    else{
+                        console.log("Last comment is from CIT");
+                    }
+                    match = r.exec(data.table);                        
+                });
             }
             //sendResponse({output: keys});
             if (keys.length>0)
