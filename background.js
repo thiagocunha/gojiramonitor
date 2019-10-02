ebf61653266893cf134d3faec75de35b9b3f3957
Thiago Chierici Cunha <tchierici@lnxcit019242.cit>
2019-10-02 10:52:35 -0300

    first almost stable version

--- background.js
+++ background.js
@@ -1,63 +1,6 @@
 "use strict";
 
-var genHandler = function (request, sender, sendResponse) {
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-        var tab = {};
-        for (var i=0;i<tabs.length;i++){
-            tab = parseInt(tabs[i].id);
-            break;
-        }
-        
-        console.log(request.type);
-        console.log(tab);
-        chrome.tabs.sendMessage(tab, {action: request.type, data: request.data}, function (response) {
-            console.log("callback 1");
-            if (!response)
-                console.log(chrome.runtime.lastError.message);
-            if (response){
-                console.log("callback 2");
-                console.log(response);
-                if (response.notifyTotal){
-                    console.log("callback 2a");
-                    chrome.browserAction.setBadgeText({text: response.notifyTotal});
-                }
-            }
-
-            console.log("callback 3");
-            
-            if (sendResponse){
-                console.log("cb4");
-            
-                sendResponse(response);
-            }
-            console.log("callback 5");
-            
-        });
-    });
-};
-
-const requestTypes = {
-	open: genHandler,
-    notify: genHandler,
-    alarm: genHandler,
-    crawler: genHandler};
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log(request.type);
-
-    if (request.type == "notify"){
-        chrome.browserAction.setBadgeText({text: request.notifyTotal});
-    }
-    else if (request.type in requestTypes) {
-        console.log("calling");
-
-        
-        
-        //genHandler(request, sender, sendResponse);
-    }
- 
-    return true;
-});
+var ticketList = [];
 
 
 chrome.alarms.create("jiraMon", {
@@ -81,12 +24,21 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
             'S466884', 
             'O53275'
         ];
+
+        // Request the comments page for the current ticket
         $.ajax("https://jira.coke.com/jira/rest/gadget/1.0/issueTable/filter?num=10&tableContext=jira.table.cols.dashboard&addDefault=false&columnNames=issuekey&columnNames=summary&columnNames=lastViewed&enableSorting=true&paging=true&showActions=true&filterId=31627&sortBy=&startIndex=0&_=1569954812196").done(function( data ) {
-            // chrome.runtime.sendMessage({type: "notify", notifyTotal: indice.toString()});
             var r = new RegExp(/(?:issuekey=.(AEM-\d*))/, 'gm');
-            var keys = [];
+            var keys = [];    
+            
+            // Check if the last comment is from CI&T, if not, include in the list
+            checkComments(r, data, koidsCIT, keys);
+            
+        });
+
+        function checkComments(r, data, koidsCIT, keys){
             var match = r.exec(data.table);
-            while(match !== null){
+            ticketList = [];
+            if(match !== null){
                 var ticket = match[1];
                 console.log(ticket);
                 $.ajax("https://jira.coke.com/jira/browse/"+ticket+"?page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel&_=1569958096799").done(function( dataComments ) {
@@ -97,21 +49,32 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
                         lastKOID = matchComments[1]
                         matchComments = regComments.exec(dataComments);
                     }
-                    if (koidsCIT.includes(lastKOID)){
-                        keys.push(ticket);
-                        console.log("Last comment not from CIT");
+                    if (koidsCIT.includes(lastKOID))
+                    {
+                        if (!keys.includes(ticket)){
+                            keys.push(ticket);
+                            ticketList.push(ticket);
+                            console.log("Last comment not from CIT");
+                        }
                     }
-                    else{
+                    else
+                    {
                         console.log("Last comment is from CIT");
-                    }
-                    match = r.exec(data.table);                        
+                    }    
+                    
+                    
+                    if (keys.length>0)
+                        chrome.browserAction.setBadgeText({text: ""+keys.length});
+                    else
+                        chrome.browserAction.setBadgeText({text: ""});
+                    
+                    chrome.storage.local.set({keys: keys.join("|")}, function() {
+                        console.log('Value is set to ' + keys);
+                    });
                 });
+                
+                checkComments(r, data, koidsCIT, keys);
             }
-            //sendResponse({output: keys});
-            if (keys.length>0)
-                chrome.browserAction.setBadgeText({text: ""+keys.length});
-            else
-                chrome.browserAction.setBadgeText({text: ""});
-        });
+        }
     }
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,62 +1,12 @@
 
-let tool = document.getElementById('run');
-
-tool.addEventListener('click', () => {
-
-	chrome.tabs.query({currentWindow: true}, function(tabs) {
-		console.log("query");
-		let txt = document.getElementById('text');
-		let url = tabs[0].url;
-		chrome.runtime.sendMessage({type: "crawler", data: document.getElementById('txtLinhas').value}, (response) => {
-			console.log(response);
-			if (response && response.output)
-				document.getElementById('txtResultado').value = response.output.join("\n");
-
-			txt.className = 'success';
-			txt.innerHTML = 'Connected';
-			
-			return true;
+$(window).focus(function(e) {
+    chrome.storage.local.get(['keys'], function(result) {
+		// Clearing the container
+		$('#ulLista').html("");
+		var lista = result.keys.split("|");
+		lista.forEach(function (item){
+			// One link for each ticket that needs atention
+			$('#ulLista').append("<il><a target='_blank' href='https://jira.coke.com/jira/browse/"+item+"'>"+item+"</a></il>");
 		});
-	});
-});
-/*
-tool = document.getElementById('auth');
-tool.addEventListener('click', () => {
-
-	
-		console.log('fetch btn press');
-		chrome.identity.getAuthToken({interactive: true}, function(token) {
-		  console.log(token);
-		  //GET DATA HERE WITH FETCH()??
-		});
-	  
-});
-*/
-tool = document.getElementById('open');
-
-tool.addEventListener('click', () => {
-
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		console.log("query");
-		let txt = document.getElementById('text');
-		let url = tabs[0].url;
-		if (url.match(/jira.domain.com/)) {
-			chrome.runtime.sendMessage({type: "notify"}, (response) => {
-		
-				txt.className = 'success';
-				txt.innerHTML = 'Connected';
-				
-				return true;
-			});
-		} else {
-			chrome.runtime.sendMessage({type: "notify"}, (response) => {
-		
-				txt.className = 'warning';
-				txt.innerHTML = 'Incorrect Url';
-				
-				return true;
-			});
-			
-		}
 	});
 });
