66a59eb30450620faeb948ecb8a11e10c8de1626
Thiago Chierici Cunha <tchierici@lnxcit019242.cit>
2019-10-02 21:32:03 -0300

    Creating options page

--- background.js
+++ background.js
@@ -11,37 +11,44 @@ chrome.alarms.create("jiraMon", {
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
     if (alarm.name === "jiraMon") {
-        var koidsCIT = [
-            'S519148', 
-            'S511003', 
-            'S520108', 
-            'S492930', 
-            'S389051', 
-            'S385334', 
-            'S519789', 
-            'S385334', 
-            'S278379', 
-            'S466884', 
-            'O53275'
-        ];
+        var koidsCIT = [];
+        var baseURL = 'https://jira.*.com/';
 
-        // Request the comments page for the current ticket
-        $.ajax("https://jira.coke.com/jira/rest/gadget/1.0/issueTable/filter?num=10&tableContext=jira.table.cols.dashboard&addDefault=false&columnNames=issuekey&columnNames=summary&columnNames=lastViewed&enableSorting=true&paging=true&showActions=true&filterId=31627&sortBy=&startIndex=0&_=1569954812196").done(function( data ) {
-            var r = new RegExp(/(?:issuekey=.(AEM-\d*))/, 'gm');
-            var keys = [];    
-            
-            // Check if the last comment is from CI&T, if not, include in the list
-            checkComments(r, data, koidsCIT, keys);
-            
+        chrome.storage.sync.get({
+            ids: '',
+            baseURL: baseURL
+            }, function(items) {
+            //document.getElementById('ids').value = items.ids;
+            //document.getElementById('baseURL').value = items.baseURL;
+            // Request the comments page for the current ticket
+            koidsCIT = items.ids.split("\n");
+            baseURL = items.baseURL;
+            $.ajax({
+                statusCode: {
+                    400: function() {
+                        chrome.browserAction.setBadgeText({text: "Auth"});
+                    }
+                },
+                url: baseURL + "jira/rest/gadget/1.0/issueTable/filter?num=10&tableContext=jira.table.cols.dashboard&addDefault=false&columnNames=issuekey&columnNames=summary&columnNames=lastViewed&enableSorting=true&paging=true&showActions=true&filterId=31627&sortBy=&startIndex=0&_=1569954812196"
+            }).done(function( data ) {
+                var r = new RegExp(/(?:issuekey=.(AEM-\d*))/, 'gm');
+                var keys = [];    
+                
+                // Check if the last comment is from the list of internal employees if not, include in the list
+                checkComments(r, data, koidsCIT, keys, baseURL);
+                
+            });
         });
 
-        function checkComments(r, data, koidsCIT, keys){
+        
+
+        function checkComments(r, data, koidsCIT, keys, baseURL){
             var match = r.exec(data.table);
             ticketList = [];
             if(match !== null){
                 var ticket = match[1];
                 console.log(ticket);
-                $.ajax("https://jira.coke.com/jira/browse/"+ticket+"?page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel&_=1569958096799").done(function( dataComments ) {
+                $.ajax(baseURL + "jira/browse/"+ticket+"?page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel&_=1569958096799").done(function( dataComments ) {
                     var regComments = new RegExp(/(?:alt=.*?"(\w\d{5}).*?added)/, 'gm');
                     var matchComments = regComments.exec(dataComments);
                     var lastKOID = "";
--- popup/popup.js
+++ popup/popup.js
@@ -2,7 +2,7 @@
 $(window).focus(function(e) {
     chrome.storage.local.get(['keys'], function(result) {
 		// Clearing the container
-		$('#ulLista').html("");
+		$('#ulLista').html("Nothing yet");
 		var lista = result.keys.split("|");
 		lista.forEach(function (item){
 			// One link for each ticket that needs atention
