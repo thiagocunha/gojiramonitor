1703807305c8b3469358bc3355998dbf0cd7003e
Thiago Chierici Cunha <tchierici@lnxcit019242.cit>
2019-10-14 21:40:43 -0300

    Revert "Parameterizing links base URL on popup"
    This reverts commit d18c27aabc990f5de6d88ff4f942399eb39d7544.

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,12 @@
 
 $(window).focus(function(e) {
-    chrome.storage.local.get(['keys', 'baseURL'], function(result) {
+    chrome.storage.local.get(['keys'], function(result) {
 		// Clearing the container
 		$('#ulLista').html("Nothing yet");
 		var lista = result.keys.split("|");
 		lista.forEach(function (item){
 			// One link for each ticket that needs atention
-			$('#ulLista').append("<il><a target='_blank' href='"+result.baseURL+"jira/browse/"+item+"'>"+item+"</a></il>");
+			$('#ulLista').append("<il><a target='_blank' href='https://jira.coke.com/jira/browse/"+item+"'>"+item+"</a></il>");
 		});
 	});
 });
